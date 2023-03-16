354ff0ba55911944159606d05dc07d661eb5cd15
casalgado <casalgado86@gmail.com>
2023-03-16 07:16:57 -0500

    bakery_orders_task

--- X_browser/example_app/script.js
+++ X_browser/example_app/script.js
@@ -10,3 +10,34 @@ function goToPosition(event) {
   event.target.style.left = `${newX}px`;
   event.target.style.top = `${newY}px`;
 }
+
+/*
+  Hola! Si encontraron esto son unos capos :)
+  Asi debe quedar el task de bakery orders.
+
+  {
+  'carlos salgado': 55000,
+  'camilo zuluaga': 57000,
+  'juliana zuluaga': 115000,
+  'alejandro zuluaga': 20000,
+  'alejandro aguilar': 100000,
+  'sebastian cotes': 63000,
+  'el pibe valderrama': 125000,
+  'gustavo cerati': 32000,
+  'diomedes diaz': 80000
+  }
+
+  {
+  'original grande': 420000,
+  'pan integral y avena grande': 50000,
+   domicilio: 30000,
+  'pan de zaatar pequeño': 40000,
+  'pan integral pequeño': 40000,
+  'pan de hamburguesa': 12000,
+  'fermento de pimenton': 15000,
+  'pan de queso costeño pequeño': 20000,
+  'pan de masa madre pequeño': 14000,
+  'pan de bono': 6000
+  }
+  
+*/
