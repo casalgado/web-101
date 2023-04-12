4b193377c58ea85b98e5124fb29a9539ac3fbc02
Carlos <casalgado86@gmail.com>
2023-04-11 20:12:11 -0500

    quiz 2

---
+++ 5_review/map.js
@@ -0,0 +1,27 @@
+let people = [
+  { name: "beto", age: 36 },
+  { name: "juli", age: 28 },
+  { name: "chami", age: 18 },
+];
+
+let skippers = [
+  { name: "alejandro", age: 32 },
+  { name: "aguilar", age: 20 },
+  { name: "cotes", age: 88 },
+];
+
+function square(num) {
+  return num * num;
+}
+
+function capitalize(name) {
+  return name.toUpperCase();
+}
+
+// console.log(skippers.map((element) => element.age));
+
+// console.log(capitalize({ name: "beto", age: 36 }));
+
+let first_name = "chami";
+
+console.log(`my name is ${first_name}`);
--- 5_review/quiz.js
+++ 5_review/quiz.js
@@ -7,10 +7,33 @@ function listNames(array) {
   }
 }
 
-listNames(employee_data);
+let example_employee = {
+  personal_id: "21-542-3562",
+  first_name: "Dosi",
+  last_name: "Gateman",
+  email: "dgateman2q@networksolutions.com",
+  gender: "Female",
+  latitude: 40.5931238,
+  longitude: 43.7385844,
+  birthdate: "2005-12-25",
+  balance: 2746419,
+  job_title: "Legal Assistant",
+};
 
-function averageBalanceByGender() {
-  // a function that recieves a gender and outputs the average balance in accounts of that gender.
+let other_employee = {
+  name: "beto",
+  balance: 15121321,
+};
+
+// to solve these, use filter(), map()
+
+console.log(namesOfEmployeesWithMoreThan(9980000));
+
+function namesOfEmployeesWithMoreThan(amount) {
+  // return the names of employees with more than an amount balance
+  return employee_data
+    .filter((e) => e.balance > amount)
+    .map((e) => `${e.first_name} ${e.last_name}`);
 }
 
 function listTopTenRichesJobs() {
@@ -18,12 +41,16 @@ function listTopTenRichesJobs() {
   // hint (has to use array sort)
 }
 
+function averageBalanceByGender() {
+  // a function that recieves a gender and outputs the average balance in accounts of that gender.
+}
+
 function namesOfEmployeesInSouthAmerica() {
   // outputs an array of names of employees in south america
 }
 
 function employeesInDateRange() {
-  // a function that takes in two dates and returns employees that were born between those dates
+  // a function that takes in two years and returns employees that were born between those years
 }
 
 function employeesOfAge() {
